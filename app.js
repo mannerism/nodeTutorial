a70281ec072ad0b590b7ad322b4cf2d329cc33e5
Juno <shy12@case.edu>
2021-09-07 15:03:16 +0900

    async await

--- app.js
+++ app.js
@@ -1,17 +1,58 @@
-// local dependency
-// npm i <packageName>
+// 가장 깔끔한 방법
+const { readFile, writeFile } = require("fs").promises;
 
-// global dependency
-// npm install -g <packageName>
-// sudo npm install -g <packageName> (mac)
+const start = async () => {
+  try {
+    const first = await readFile("./content/first.txt", "utf8");
+    const second = await readFile("./content/second.txt", "utf8");
+    await writeFile(
+      "./content/result-mind-grenade.txt",
+      `THIS IS AWESOME: ${(first, second)}`
+    );
+    console.log(first, second);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-// package.json - manifest file (stores important info about project/package)
-// manual approach (create package.json in the root, create properties etc)
-// npm init (step by step, press enter to skip)
-// npm init -y (everything default)
+start();
 
-const _ = require("lodash");
+// 가장 복잡한 케이스 1.
+// getText("./content/first.txt")
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
 
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
+// 이 부분이 저 위에 `readFilePromise, writeFilePromise`를 from the scratch로 만들었을때 작성해야하는 코드 이다.
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
+
+// 그 다음 복잡한 케이스 2.
+// const { readFile, writeFile } = require("fs");
+// const util = require("util");
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
+
+// const start = async () => {
+//   try {
+//     const first = await readFilePromise("./content/first.txt", "utf8");
+//     const second = await readFilePromise("./content/second.txt", "utf8");
+//     await writeFilePromise(
+//       "./content/result-mind-grenade.txt",
+//       `THIS IS AWESOME: ${(first, second)}`
+//     );
+//     console.log(first, second);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// start();
